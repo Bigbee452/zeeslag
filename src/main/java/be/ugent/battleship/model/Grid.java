16ff349bb6c90c4093c56fc3da6ace143e3ba9ac
bigbee <silvandriessche@gmail.com>
2025-11-14 13:06:44 +0100

    added cells to grid

--- src/main/java/be/ugent/battleship/model/Grid.java
+++ src/main/java/be/ugent/battleship/model/Grid.java
@@ -1,7 +1,19 @@
 package be.ugent.battleship.model;
 
 public class Grid {
-    public Grid(int breedte, int hoogte){
+    private int breedte;
+    private int hoogte;
+    private Cell[][] cells;
 
+    public Grid(int breedte, int hoogte){
+        this.breedte = breedte;
+        this.hoogte = hoogte;
+        cells = new Cell[hoogte][breedte];
+        for(int i = 0; i < hoogte; i++){
+            for(int j = 0; j < breedte; j++){
+                Position pos = new Position(i, j);
+                cells[i][j] = new Cell(pos);
+            }
+        }
     }
 }
